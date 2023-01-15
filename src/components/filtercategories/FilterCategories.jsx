b96412fbf38de1abbfbfcd3d6c99d84832043936
mdarif6 <md1arif015@gmail.com>
2023-01-16 00:13:04 +0530

    small bug fixed

--- src/components/filtercategories/FilterCategories.jsx
+++ src/components/filtercategories/FilterCategories.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { useCart } from "../../context/cart-context";
 
 export default function FilterCategories({ category, categoryHandle }) {
+  const { state } = useCart();
+  const categoryCheckedInArray =
+    state.filterByCategories.indexOf(category) !== -1;
   return (
     <div className="flipcart-filter-sorting-categories">
       <label>
-        <input type="checkbox" onChange={categoryHandle} />
+        <input
+          type="checkbox"
+          checked={categoryCheckedInArray}
+          onChange={categoryHandle}
+        />
         {category}
       </label>
     </div>
--- src/components/filtersize/FilterSize.jsx
+++ src/components/filtersize/FilterSize.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useCart } from "../../context/cart-context";
 
 export default function FilterSize({ size, sizeHandle }) {
+  const { state } = useCart();
+  const sizeCheckedInArray = state.filterBySize.indexOf(size) !== -1;
   return (
     <div className="flipcart-filter-sorting-categories">
       <label>
-        <input type="checkbox" onChange={sizeHandle} />
+        <input
+          type="checkbox"
+          checked={sizeCheckedInArray}
+          onChange={sizeHandle}
+        />
         {size}
       </label>
     </div>
