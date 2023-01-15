c935cd85679f9ac2e12b97be86a6f414a6546707
mdarif6 <md1arif015@gmail.com>
2023-01-15 23:30:46 +0530

    Import Relatged Bug Fixed

--- src/pages/productpage/ProductsPage.jsx
+++ src/pages/productpage/ProductsPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Aside from "../aside/Aside";
-import AsideMobile from "../aside/AsideMobile";
-import Main from "../main/Main";
+import Aside from "../../components/aside/Aside";
+import AsideMobile from "../../components/aside/AsideMobile";
+import Main from "../../components/main/Main";
 import "./ProductsPage.css";
 export default function ProductsPage() {
   const [showSideBar, setShowSideBar] = useState(false);
